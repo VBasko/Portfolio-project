5af7bb273ecb5f9d7de71c23cf9cf788d3a8c8f4
VBasko <106957194+VBasko@users.noreply.github.com>
2022-08-10 04:30:41 +0400

    Realized changing active link

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Add shadow to header when scrolling
 const header = document.getElementById("header");
 
 window.onscroll = function () {
@@ -7,7 +8,49 @@ window.onscroll = function () {
 function headerActive() {
   if (document.documentElement.scrollTop > 0) {
     header.classList.add("active");
+    activeNavLink();
   } else {
     header.classList.remove("active");
   }
 }
+
+// Change active nav-link when scrolling
+const sections = document.querySelectorAll("section"),
+  homeSection = document.getElementById("home"),
+  navLinks = document.querySelectorAll("header .nav-links .nav-link");
+function activeNavLink() {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (document.documentElement.scrollTop < 300) {
+      current = "home";
+    } else if (
+      document.documentElement.scrollTop >=
+      sectionTop - section.offsetHeight / 2 - 30
+    ) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.classList.contains(current)) {
+      link.classList.add("active");
+    }
+  });
+}
+
+// Show menu
+const openMenu = document.getElementById("menu-btn"),
+  closeMenu = document.getElementById("close-btn"),
+  navMenu = document.getElementById("nav-menu");
+
+openMenu.addEventListener("click", () => {
+  navMenu.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+});
